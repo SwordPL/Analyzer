306cd97bc634338809234829148ee686dde8fc22
Hubert Pomorski <pomorski.hubert@gmail.com>
2015-07-26 00:17:10 +0200

    Bug-fix: createGraph method was not able to create graph with filtered classes;
    Feature: Graph object is now LCC

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jgrapht.DirectedGraph;
+import org.jgrapht.alg.ConnectivityInspector;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.pf.tools.cda.base.model.ClassInformation;
@@ -10,14 +11,12 @@ import org.pf.tools.cda.base.model.workset.ClasspathPartDefinition;
 import org.pf.tools.cda.core.init.WorksetInitializer;
 import org.pfsw.odem.TypeClassification;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Main {
-    static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static String userPattern;
 
     public static void main(String[] args) {
         logger.info("Welcome to Class Dependency Analyser by usage of networks.");
@@ -35,17 +34,21 @@ public class Main {
         logger.info(".* -- All packages");
         logger.info("java.(.*) - All packages starting with Java");
 
-        String userPattern = sc.nextLine();
-        List<ClassPackage> filtered = filterPackages(workset, userPattern);
+        userPattern = sc.nextLine();
+        List<ClassPackage> filteredPackages = filterPackages(workset, userPattern);
 
         logger.info("PHASE 5: CREATING DEPENDENCY GRAPH");
-        List<ClassInformation> classes = getClassesInfo(filtered);
+        List<ClassInformation> classes = getClassesInfo(filteredPackages);
         DirectedGraph<ClassInformation, DefaultEdge> graph = createGraph(classes);
+
+        logger.info("PHASE 6: CHOOSING Largest Connected Component");
+        Set<ClassInformation> lccSet = getLCCSet(graph);
+        graph = createGraph(lccSet);
     }
 
     private static Workset createWorkset(String classpath) {
         logger.info("PHASE 1: WORKSET CREATION");
-        Workset workset = new Workset("Testing");
+        Workset workset = new Workset("Analyzer");
         ClasspathPartDefinition partDefinition = new ClasspathPartDefinition(classpath);
         workset.addClasspathPartDefinition(partDefinition);
         return workset;
@@ -57,8 +60,8 @@ public class Main {
         wsInitializer.initializeWorksetAndWait(new ProgressMonitor());
     }
 
-    private static List<ClassPackage> filterPackages(Workset workset, String userPattern) {
-        Pattern pattern = Pattern.compile(userPattern);
+    private static List<ClassPackage> filterPackages(Workset workset, String uPattern) {
+        Pattern pattern = Pattern.compile(uPattern);
         PackageNameFilter filter = new PackageNameFilter(pattern);
         List<ClassPackage> result = new ArrayList<>();
         workset.processClassPackageObjects(result, filter);
@@ -73,15 +76,32 @@ public class Main {
         return classes;
     }
 
-    private static DirectedGraph<ClassInformation, DefaultEdge> createGraph(List<ClassInformation> classes) {
+    private static DirectedGraph<ClassInformation, DefaultEdge> createGraph(Collection<ClassInformation> classes) {
         DirectedGraph<ClassInformation, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+        if (classes == null) return g;
+
+        Pattern pattern = Pattern.compile(userPattern);
         classes.forEach(g::addVertex);
         classes.forEach(classInfo -> classInfo.getDirectReferredTypes()
                         .stream()
-                        .filter(dependency -> dependency.getClassification() == TypeClassification.CLASS)
-                        .forEach(dependency -> g.addEdge(classInfo, (ClassInformation) dependency))
+                        .filter(dependency -> dependency.getClassification() != TypeClassification.UNKNOWN)
+                        .map(dependency -> (ClassInformation) dependency)
+                        .filter(dependency -> pattern.matcher(dependency.getClassName()).matches())
+                        .forEach(dependency -> g.addEdge(classInfo, dependency))
         );
-
         return g;
     }
+
+    private static Set<ClassInformation> getLCCSet(DirectedGraph<ClassInformation, DefaultEdge> graph) {
+        ConnectivityInspector<ClassInformation, DefaultEdge> inspector
+                = new ConnectivityInspector<>(graph);
+        List<Set<ClassInformation>> sets = inspector.connectedSets();
+        Set<ClassInformation> lccVertices = null;
+        for (Set<ClassInformation> set : sets) {
+            if (lccVertices == null || lccVertices.size() < set.size())
+                lccVertices = set;
+        }
+        return lccVertices;
+    }
+
 }
